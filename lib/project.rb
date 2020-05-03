231806ea0cf673c9cf5974699f0b403e8fb9f809
Koray Ozkal <koryozkal@gmail.com>
2020-05-03 00:03:18 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,14 @@
 class Project
   attr_reader :title, :backers
-  
+
   def initialize(title)
     @title = title
-    @backers= []
-  end 
-    def add_backer(backer) 
-    @backers<< backers
-    self.backed_projects << self 
+    @backers = []
+  end
 
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
 end 
 end
